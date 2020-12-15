1d08e3741e6f9049d80b3dd10fbf9f23c974b850
Coby <cobyrpilpel2@gmail.com>
2020-12-15 01:36:31 -0500

    commit tree e84663a1477713dc13e0b298dfa188a0294b0ef1

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.count == 1 
+        then array.join
+    elsif array.count == 2 then array.join(" and ")
+    elsif array.count >= 3
+        array[-1].insert( 0 , "and ")
+        ##array[-1].insert (" and ")  ##go back and re-read. Why does this not work/ why does the 0 work.  "MyCodeWorksIHaveNoIdeaWhy.meme"
+        array.join(', ')
+    end
 end
+
